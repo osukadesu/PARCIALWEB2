c9cc421074421b90bbf1f4d0dbaa0f1310718abc
osukadesu <64621867+osukadesu@users.noreply.github.com>
2020-11-12 20:03:08 -0500

    Update EstudianteModel.cs

--- parcial2/Models/EstudianteModel.cs
+++ parcial2/Models/EstudianteModel.cs
@@ -2,6 +2,27 @@ namespace parcial2.Models
 {
     public class EstudianteModel
     {
+        {
+
+        public string IdEstudiante { get; set; }
         
+        public string NombreAcudiente { get; set; }
+
+        public string Apellido { get; set; }
+        }
+
+    public class EstudianteViewModel : EstudianteInputModel
+    {
+        public EstudianteViewModel(Estudiante estudiante)
+        {
+            Cedula = estudiante.Cedula;
+            Nombre = estudiante.Nombre;
+            Apellido = estudiante.Apellido;
+            Email = estudiante.Email;
+            FechaNacimiento = estudiante.FechaNacimiento;
+            Sexo = estudiante.Sexo;
+            Telefono = estudiante.Telefono;
+            Ciudad = estudiante.Ciudad;
+        }
     }
 }
